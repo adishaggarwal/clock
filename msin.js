0dc220a14698a90d4fa3ef24a0c07a080bf933b8
adish aggarwal <aggarwaladish@gmail.com>
2019-10-04 21:43:52 +0530

    kjbjk

--- msin.js
+++ msin.js
@@ -1,5 +1,25 @@
 
 $(document).ready(function(){
+    dup();
+});
+
+setInterval(function() {
+    var d=new Date();
+    var h=d.getHours();
+    var m=d.getMinutes();
+    var s=d.getSeconds();
+    if(h==0 && s==0 && m==0)
+    {
+        dup();
+    }
+    
+    else
+    $(".time").html(`<p>${h}:${m}:${s}</p>`);
+}, 1000);
+
+
+function dup()
+{
     var d=new Date();
     var h=d.getHours();
     var m=d.getMinutes();
@@ -13,27 +33,4 @@ $(document).ready(function(){
     
     $(".date").html(`<p>${day}, ${num} ${month}</p>`);
     $(".time").html(`<p>${h}:${m}:${s}</p>`);
-});
-
-setInterval(function() {
-    var d=new Date();
-    var h=d.getHours();
-    var m=d.getMinutes();
-    var s=d.getSeconds();
-    if(h==0 && s==0 && m==0)
-    {
-        var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-
-var day = days[ d.getDay()];
-var month = months[ d.getMonth() ];
-var num=d.getDate();
-
-$(".date").html(`<p>${day}, ${num} ${month}</p>`);
-$(".time").html(`<p>${h}:${m}:${s}</p>`);
-    }
-    
-    else
-    $(".time").html(`<p>${h}:${m}:${s}</p>`);
-}, 1000);
-
+}
